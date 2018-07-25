cdb372035e999ff60494f495f8143ecc9c4482b3
Sandra <Sandra@Sandra-Tu.local>
2018-07-25 17:23:53 +0800

    Initial commit

---
+++ ZH/smushZH.py
@@ -0,0 +1,60 @@
+import re
+
+def hasNext(lines):
+    pass
+
+if __name__ == "__main__":
+    inputFile = "file.txt"
+    dictFile = "zh-dict.txt"
+    endErrors = []
+    startErrors = []
+    dict = {}
+    with open (dictFile,"r",encoding = "utf-8") as file:
+        for _ in list(filter(None, file.read().split("\n"))):
+            if _[-1] in dict:
+                dict[_[-1]].add(_[0])
+            else:
+                dict[_[-1]] = set(_[0])
+
+    with open ("breakErrorsEnd.txt","r",encoding = "utf-16") as file:
+        endErrors = file.read().split("\n")
+
+    with open ("breakErrorsStart.txt","r",encoding = "utf-16") as file:
+        startErrors = file.read().split("\n")
+
+    with open (inputFile,"r",encoding = "utf-16") as file:
+        lines = []
+        for _ in list(filter(None, file.read().split("\n"))):
+            lines.append(_)
+
+        for count in range(len(lines)-1,1,-1):
+            line = lines[count]
+            if line[0] in dict:
+                if lines[count-1][-1] in dict[line[0]]:
+                    lines[count-1] = "".join([lines[count-1],line])
+                    lines[count] = ""
+
+        lines = list(filter(None, lines))
+
+        for count in range(0,len(lines)-2):
+            line = lines[count]
+            if line[-1] in endErrors:
+                lines[count+1] = "".join([line,lines[count+1]])
+                lines[count] = ""
+
+        lines = list(filter(None, lines))
+
+        for count in range(len(lines)-1,1,-1):
+            line = lines[count]
+            if line[0] in startErrors:
+                lines[count-1] = "".join([lines[count-1],line])
+                lines[count] = ""
+
+        lines = list(filter(None, lines))
+
+        for _ in lines:
+            _.replace(" ","")
+
+    with open("alignmentSorted.txt","w",encoding = "utf-8") as file:
+        for _ in lines:
+            file.write(_ + "\n")
